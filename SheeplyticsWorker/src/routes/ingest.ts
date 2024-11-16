ca8c74dbae9c17d90ab1955592f1121de8449449
SplittyDev <splittydev@protonmail.com>
2024-11-16 11:21:01 +0700

    worker: Implement batched event handling

--- SheeplyticsWorker/src/routes/ingest.ts
+++ SheeplyticsWorker/src/routes/ingest.ts
@@ -5,35 +5,59 @@ import SimilarityCache from "../similarity_cache"
 import type { BaseEvent, TypedEvent, EventKind } from "../types"
 
 type IngestResponseKind = 'acknowledged' | 'rejected'
+type InsertionResultKind = 'success' | 'cached' | 'failure'
 
 type IngestResponse = {
 	message: IngestResponseKind
 	status: number
 }
 
-/** Ingestion route for analytics events. */
-export default async function handler(request: IRequest, env: Env): Promise<IngestResponse> {
-	const json = await request.json()
+async function handleEventPayload(payload: unknown, database: Database, cache: SimilarityCache, env: Env): Promise<InsertionResultKind> {
 
 	// Decode specific event data
-	const baseEvent = json as BaseEvent
+	const baseEvent = payload as BaseEvent
 	const innerEvent: Record<string, unknown> = JSON.parse(atob(baseEvent.data))
 	const typedEvent: TypedEvent<EventKind, unknown> = { ...baseEvent, data: innerEvent }
 
 	// Check if event is the same as another recent event
-	const cache = new SimilarityCache(env.SIMILARITY_CACHE)
 	if (await cache.exists(typedEvent)) {
-		return { message: 'rejected', status: 409 }
+		return 'cached'
 	}
 
 	// Insert event into database
 	try {
-		const database = new Database(env.ANALYTICS_DB)
 		await database.insertEvent(typedEvent)
 		await cache.put(typedEvent)
-		return { message: 'acknowledged', status: 200 }
-	} catch(err) {
-		console.log(err)
-		return { message: 'rejected', status: 500 }
+		return 'success'
+	} catch {
+		return 'failure'
+	}
+}
+
+/** Ingestion route for analytics events. */
+export default async function handler(request: IRequest, env: Env): Promise<IngestResponse> {
+	const json = await request.json()
+
+	// Initialize database and cache
+	const database = new Database(env.ANALYTICS_DB)
+	const cache = new SimilarityCache(env.SIMILARITY_CACHE)
+
+	// Handle batched events
+	const isBatch = Array.isArray(json)
+	const events = isBatch ? json : [json]
+	const results: InsertionResultKind[] = []
+
+	// Insert each event into the database
+	for (const event of events) {
+		results.push(await handleEventPayload(event, database, cache, env))
+	}
+
+	// A batch is considered rejected if all events are rejected
+	const allRejected = results.every(result => result === 'failure')
+
+	// Return response
+	return {
+		message: allRejected ? 'rejected' : 'acknowledged',
+		status: allRejected ? 500 : 200
 	}
 }
